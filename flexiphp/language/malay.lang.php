0dcdd625f6fb1ba46607b3959b5524b88bedbf74
James <james@mercstudio.com>
2010-11-12 11:41:03 +0800

    init version

---
+++ flexiphp/language/malay.lang.php
@@ -0,0 +1,17 @@
+<?php
+/* 
+ * All keys must be in small-cap or exactly the same text
+ */
+$aLanguage = array(
+  "field is required" => "tempat kosong ini perlu di isi",
+  "field must be an email" => "tempat kosong ini mesti mengandungi e-mel sahaja",
+  "unknown mailer type" => "e-Mel konfigurasi yang meragukan",
+  "email error" => "masalah e-Mel",
+  "please contact administrator" => "sila hubungi kami",
+  "login form" => "borang login",
+  "thank you for your request" => "terima kasih",
+  "your request has been submitted" => "borang anda sudah di hantar",
+  "We will get back to you within 24 working hours" => "kami akan menghubungi anda secepat mungkin",
+  "Click here to return" => "sila klik di sini untuk kembali ke halaman",
+  "thank you" => "terima kasih"
+);
