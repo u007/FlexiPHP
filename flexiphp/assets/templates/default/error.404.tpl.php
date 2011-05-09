264af2d1cb0f36fcdeb6fab234d13b8bb914fe03
james <james@192.168.0.113>
2011-05-09 18:01:10 +0800

    show missing file

--- flexiphp/assets/templates/default/error.404.tpl.php
+++ flexiphp/assets/templates/default/error.404.tpl.php
@@ -1,11 +1,7 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 ?>
 <h3><?=flexiT("Error", "first")?> 404</h3>
-<div><?=flexiT("sorry, path / file not found","first")?>.</div>
+<div><?=flexiT("sorry, path " . $vars["path"] . " file not found","first")?>.</div>
 <div><?=flexiT("please contact site administrator", "first")?></div>
 <div><a href=""><?=flexiT("Click here to return", "first")?></a></div>
