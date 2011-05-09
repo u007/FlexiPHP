83d3f1cf5f0009e61c0d8c1a832364b7fc9e1317
james <james@192.168.0.113>
2011-05-09 18:04:52 +0800

    new formitem to render form, and output, to be used for resetform

--- flexiphp/assets/templates/default/tab-form.tpl.php
+++ flexiphp/assets/templates/default/tab-form.tpl.php
@@ -1,46 +1,18 @@
 <?php
 extract($vars);
 
-
+$sFormItem = $this->render("tab-formitem");
 ?>
 <form id="<?=$sViewDBFormPrefix?>frmObject" name="frmObject" class="uniForm" method="POST" action="<?=$sSaveURL?>" enctype="multipart/form-data">
-  <input type="hidden" name="txtFormType" value="<?=$formtype?>" />
-  <div id="<?=$sViewDBFormPrefix?>divFrmObject">
-    <div class="ctrlHolderTitle" >
-      New Record
-    </div>
-<? foreach($aFieldsInput as $sName => $aForm) {
-    if (!empty($aForm["label"])) { ?>
-      <div class="ctrlHolder">
-        <?=$aForm["label"]?>
-        <?=$aForm["input"]?>
-      </div>
-    <?
-    } else {
-    ?>
-      <?=$aForm["input"]?>
-    <? } ?>
-  <? } ?>
-  </div>
-        <div class="ctrlHolder">
-          <label for="" > </label>
-          <div class="subnav_btn_idle" style="float:left;width:70px;text-align:center;">
-            <input type="submit" class="btnsubmit" style="width:70px" value="Save">
-  </div>
-          <div class="subnav_btn_idle" style="margin-left: 5px; float:left;width:70px;text-align:center;">
-            <input type="button" class="btnsubmit" style="width:70px" onClick="<?=$sViewDBFormPrefix?>resetForm()" value="Cancel" />
-          </div>
-
-        </div>
-
+  <?=$sFormItem?>
 </form>
 
 <script type="text/javascript">
 var <?=$sViewDBFormPrefix?>fieldCnt = 0;
-var <?=$sViewDBFormPrefix?>sNewFormHTML = "";
+var <?=$sViewDBFormPrefix?>sNewFormHTML = <?=json_encode($sFormItem)?>;
 
 jQuery(document).ready(function() {
-  <?=$sViewDBFormPrefix?>sNewFormHTML = jQuery("#<?=$sViewDBFormPrefix?>frmObject").html();
+  //<?=$sViewDBFormPrefix?>sNewFormHTML = jQuery("#<?=$sViewDBFormPrefix?>frmObject").html();
 })
 
 function <?=$sViewDBFormPrefix?>onAfterInitForm() {
