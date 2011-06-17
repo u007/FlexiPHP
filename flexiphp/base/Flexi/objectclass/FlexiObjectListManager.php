f1339cb8cfac1ae9419f3387d68c065dd0aea480
james <james@james-asus>
2011-06-17 23:39:45 +0800

    removed debug msg

--- flexiphp/base/Flexi/objectclass/FlexiObjectListManager.php
+++ flexiphp/base/Flexi/objectclass/FlexiObjectListManager.php
@@ -121,7 +121,7 @@ class FlexiObjectListManager extends FlexiLogManager {
     $aPrimary = $oObject->getPrimaryFields();
     $this->oLastSavedRow = $oRow;
     $this->sLastSaveType = $sType;
-    echo print_r($oRow,true);
+    //echo print_r($oRow,true);
     switch($sType) {
       case "insert":
         return FlexiModelUtil::getInstance()->insertXPDO($sTable, $oRow, $aPrimary);
