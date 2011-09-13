39e353fe31a77d0d23b5d554539a4c9f1c211497
james <james@james-asus>
2011-09-13 21:42:59 +0800

    + added check for empty image + added get image size + fixed transparent issue with resize + avoid resizing if image is smaller than max width

--- flexiphp/base/Flexi/objectclass/FlexiImageUtil.php
+++ flexiphp/base/Flexi/objectclass/FlexiImageUtil.php
@@ -6,7 +6,6 @@
  */
 class FlexiImageUtil {
   
-  
   public static function imagePreview($aiWidth, $aiHeight, $sPath, $sNewPath = null) {
     $aiWidth = (int)$aiWidth;
     $aiHeight = (int)$aiHeight;
@@ -30,12 +29,17 @@ class FlexiImageUtil {
    * @param int $width
    * @param int $height
    * @param String $sPath
+   * @param int Write type
+   *  IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_PNG
    * @return <type>
    */
-  public static function imageResize($aiWidth, $aiHeight, $sPath, $sNewPath = null) {
+  public static function imageResize($aiWidth, $aiHeight, $sPath, $sNewPath = null, $aiWriteType = null) {
     if (empty($aiWidth) && empty($aiHeight)) { return; }
     list($iOriWidth, $iOriHeight, $iType) = getimagesize($sPath);
-
+    
+    if ($iOriWidth <= 0 || $iOriHeight <= 0) { 
+      throw new Exception("Unable to load image: " . $sPath);
+    }
     if (!empty($aiWidth) && !empty($aiHeight)) {
       if ($iOriWidth > $iOriHeight) {
         $percentage = ($aiWidth / $iOriWidth);
@@ -51,14 +55,21 @@ class FlexiImageUtil {
     //gets the new value and applies the percentage, then rounds the value
     $width = round($iOriWidth * $percentage);
     $height = round($iOriHeight * $percentage);
+    
+    if ($width > $iOriWidth) {
+      //too big, use original size
+      $width = $iOriWidth;
+      $height = $iOriHeight;
+    }
+    
     switch ($iType) {
-      case 1:   //   gif -> jpg
+      case IMAGETYPE_GIF:   //   gif -> jpg
         $image = imagecreatefromgif($sPath);
         break;
-      case 2:   //   jpeg -> jpg
+      case IMAGETYPE_JPEG:   //   jpeg -> jpg
         $image = imagecreatefromjpeg($sPath);
         break;
-      case 3: //   png -> jpg
+      case IMAGETYPE_PNG: //   png -> jpg
         $image = imagecreatefrompng($sPath);
         break;
       default:
@@ -66,23 +77,69 @@ class FlexiImageUtil {
     }
     FlexiLogger::info(__METHOD__, "Resize from: " . $iOriWidth . "x" . $iOriHeight . " to " . $width . "x" . $height);
     $image_p = imagecreatetruecolor($width, $height);
+    imagealphablending($image_p, false);
+    imagesavealpha($image_p, true);
+    //$transparent = imagecolorallocatealpha($image_p, 255, 255, 255, 127);
+    
     imagecopyresampled($image_p, $image, 0, 0, 0, 0, $width, $height, $iOriWidth, $iOriHeight);
-
+    
     $sWritePath = empty($sNewPath) ? $sPath : $sNewPath;
-    switch ($iType) {
-      case 1:   //   gif -> jpg
-        $image = imagegif($image_p, $sWritePath);
+    $iWriteType = empty($aiWriteType) ? $iType: (int)$aiWriteType;
+    
+    switch ($iWriteType) {
+      case IMAGETYPE_GIF:
+        $image = @imagegif($image_p, $sWritePath);
         break;
-      case 2:   //   jpeg -> jpg
-        $image = imagejpeg($image_p, $sWritePath);
+      case IMAGETYPE_JPEG:
+        $image = @imagejpeg($image_p, $sWritePath);
         break;
-      case 3: //   png -> jpg
-        $image = imagepng($image_p, $sWritePath);
+      case IMAGETYPE_PNG:
+        
+        $image = @imagepng($image_p, $sWritePath);
         break;
       default:
         throw new FlexiException("Unknown image type: " . $iType, ERROR_DATATYPE);
     }
+    
+    if ($image===false) throw new Exception("Image generation failed: " . $iWriteType . ", path: " . $sWritePath);
     return array($width, $height);
   }
+  
+  public static function getImageSize($sPath) {
+		list($iWidth, $iHeight, $iType) = getimagesize($sPath);
+		return array($iWidth, $iHeight); //wxh
+	}
+  
+  public static function imageIsEmpty($sPath) {
+		list($iWidth, $iHeight, $iType) = getimagesize($sPath);
+		switch ($iType) {
+      case IMAGETYPE_GIF:   //   gif -> jpg
+        $image = imagecreatefromgif($sPath);
+        break;
+      case IMAGETYPE_JPEG:   //   jpeg -> jpg
+        $image = imagecreatefromjpeg($sPath);
+        break;
+      case IMAGETYPE_PNG: //   png -> jpg
+        $image = imagecreatefrompng($sPath);
+        break;
+      default:
+        throw new FlexiException("Unknown image type: " . $iType, ERROR_DATATYPE);
+    }
+    //echo "width: " . $iWidth . ", height: " . $iHeight . "<br/>\n";
+    for($iRow = 0; $iRow < $iHeight; $iRow++) {
+			//echo "Loop: " . $iRow . "<br/>\n";
+			for ($iCol = 0; $iCol < $iWidth; $iCol++) {
+				//echo "Loop-x: " . $iCol . "<br/>\n";
+				$rgb = imagecolorat($image, $iCol, $iRow);
+				$colors = imagecolorsforindex($image, $rgb);
+				//echo "colors: " . print_r($colors,true) . "<br/>\n";
+				if ($colors["alpha"] <=100) { // 127 - total transparent, 0- totally appear / opaque
+					return false; //is not empty, has something visible
+				}
+			}//for x
+		}//for y
+		
+		return true; //is empty, all alpha lower than 100
+	}
 }
 ?>
